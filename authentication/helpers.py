c8952d279ebd658c045b2ff2baf3646683ed356b
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2023-10-26 20:09:24 +0530

    check linkedin access token functionality

---
+++ authentication/helpers.py
@@ -0,0 +1,22 @@
+import requests
+from django.core.exceptions import ObjectDoesNotExist
+from authentication.models import Token
+
+def check_linkedin_token():
+    token_obj = None
+    try:
+        token_obj = Token.objects.get(type="Linkedin")
+    except ObjectDoesNotExist:
+        return False, "LinkedIn token not found."
+
+    if token_obj:
+        url = "https://api.linkedin.com/v2/userinfo"
+        headers = {
+            'Authorization': f'Bearer {token_obj.token}',
+        }
+
+        response = requests.get(url, headers=headers, verify=False)
+
+        return 200 <= response.status_code < 300, response.text
+
+    return False
--- linkedin/tasks.py
+++ linkedin/tasks.py
@@ -1,24 +1,31 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 from linkedin.models import LinkedinPost
 from linkedin.helpers import get_linkedin_post
-from django.core.exceptions import ObjectDoesNotExist
+from authentication.helpers import check_linkedin_token
 
-@shared_task(name='daily_linkedin_post')
+
+@shared_task(name="daily_linkedin_post")
 def daily_linkedin_post():
     # Get today's date
     today = timezone.now().date()
 
-    try:
-        # Try to get the LinkedinPost for today
-        daily_linkedin_post = LinkedinPost.objects.get(created_at__date=today)
-        print(f"Linkedin Post Already created on {today.strftime('%B %d, %Y')}")
+    # check Linkedin access token
+    check_token, error = check_linkedin_token()
+    if check_token:
+        try:
+            # Try to get the LinkedinPost for today
+            daily_linkedin_post = LinkedinPost.objects.get(created_at__date=today)
+            print(f"Linkedin Post Already created on {today.strftime('%B %d, %Y')}")
 
-    except ObjectDoesNotExist:
-        # If post doesn't exist, then fetch the LinkedIn post and create the object
-        markdown_content = get_linkedin_post()
-        daily_linkedin_post = LinkedinPost.objects.create(created_at=today, markdown=markdown_content)
-        print("Linkedin Post Created Successfully!")
-
-        
+        except ObjectDoesNotExist:
+            # If post doesn't exist, then fetch the LinkedIn post and create the object
+            markdown_content = get_linkedin_post()
+            daily_linkedin_post = LinkedinPost.objects.create(
+                created_at=today, markdown=markdown_content
+            )
+            print("Linkedin Post Created Successfully!")
+    else:
+        print("Invalid Access Token!!")
