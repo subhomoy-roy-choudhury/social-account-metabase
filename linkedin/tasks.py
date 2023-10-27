6a3e6f82d4063a5df70a0bdc7663851412f9adda
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2023-10-27 10:01:52 +0530

    updated daily post task code

--- linkedin/tasks.py
+++ linkedin/tasks.py
@@ -13,24 +13,26 @@ def daily_linkedin_post():
     today = timezone.now().date()
 
     # check Linkedin access token
-    access_token, error = check_linkedin_token()
-    if access_token:
-        try:
-            # Try to get the LinkedinPost for today
-            daily_linkedin_post = LinkedinPost.objects.get(created_at__date=today)
-            print(f"Linkedin Post Already created on {today.strftime('%B %d, %Y')}")
-
-        except ObjectDoesNotExist:
-            # If post doesn't exist, then fetch the LinkedIn post and create the object
-            markdown_content = get_linkedin_post()
-            # Create Post
-            # linkedin_post_id = create_linkedin_post(access_token, markdown_content)
-            linkedin_post_id = "urn:li:share:6521244543193575424"
-            daily_linkedin_post = LinkedinPost.objects.create(
-                created_at=today,
-                markdown=markdown_content,
-                linkedin_post_id=linkedin_post_id,
-            )
-            print("Linkedin Post Created Successfully!")
-    else:
-        print("Invalid Access Token!!")
+    try :
+        access_token, error = check_linkedin_token()
+        if access_token:
+                # Try to get the LinkedinPost for today
+                daily_linkedin_posts = LinkedinPost.objects.filter(created_at__date=today)
+                if len(daily_linkedin_posts) > 0: 
+                    print(f"Linkedin Post Already created on {today.strftime('%B %d, %Y')}")
+                else :
+                    # If post doesn't exist, then fetch the LinkedIn post and create the object
+                    markdown_content = get_linkedin_post()
+                    # Create Post
+                    # linkedin_post_id = create_linkedin_post(access_token, markdown_content)
+                    linkedin_post_id = "urn:li:share:6521244543193575424"
+                    daily_linkedin_post = LinkedinPost.objects.create(
+                        created_at=today,
+                        markdown=markdown_content,
+                        linkedin_post_id=linkedin_post_id,
+                    )
+                    print("Linkedin Post Created Successfully!")
+        else:
+            print("Invalid Access Token!!")
+    except Exception as e:
+         print("error in daily_linkedin_post task")
