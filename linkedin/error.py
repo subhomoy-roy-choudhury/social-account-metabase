5057aa59b464ce79afe8a77f749dc001b5d3d849
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2023-10-30 09:33:03 +0530

    uncommented create post and added custom error handling

--- linkedin/error.py
+++ linkedin/error.py
@@ -1,2 +1,7 @@
 class NoSuitableArticleFound(Exception):
     pass
+
+class DailyLinkedinPostTaskError(Exception):
+    def __init__(self, message="Daily Linkedin Post Task Failed !!"):
+        self.message = message
+        super().__init__(self.message)
--- linkedin/tasks.py
+++ linkedin/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.utils import timezone
-from django.core.exceptions import ObjectDoesNotExist
+from linkedin.error import DailyLinkedinPostTaskError
 from linkedin.models import LinkedinPost
 from linkedin.helpers import get_linkedin_post, create_linkedin_post
 from authentication.helpers import check_linkedin_token
@@ -24,15 +24,16 @@ def daily_linkedin_post():
                     # If post doesn't exist, then fetch the LinkedIn post and create the object
                     markdown_content = get_linkedin_post()
                     # Create Post
-                    # linkedin_post_id = create_linkedin_post(access_token, markdown_content)
-                    linkedin_post_id = "urn:li:share:6521244543193575424"
+                    linkedin_post_id = create_linkedin_post(access_token, markdown_content)
+                    # linkedin_post_id = "urn:li:share:6521244543193575424"
                     daily_linkedin_post = LinkedinPost.objects.create(
                         created_at=today,
                         markdown=markdown_content,
                         linkedin_post_id=linkedin_post_id,
+                        is_send=True
                     )
                     print("Linkedin Post Created Successfully!")
         else:
             print("Invalid Access Token!!")
     except Exception as e:
-         print("error in daily_linkedin_post task")
+         raise DailyLinkedinPostTaskError()
