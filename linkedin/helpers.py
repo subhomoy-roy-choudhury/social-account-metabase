87e4741d0bce3a6220a03c01dbd85f2843793690
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2023-11-06 09:55:52 +0530

    added reference link for counting tokens

--- linkedin/helpers.py
+++ linkedin/helpers.py
@@ -25,7 +25,10 @@ openai.api_key = settings.OPENAI_API_KEY
 
 
 def count_tokens(string: str, model_name: str = "gpt-3.5-turbo") -> int:
-    """Returns the number of tokens in a text string."""
+    """
+    Returns the number of tokens in a text string.
+    Reference Link := https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+    """
     encoding = tiktoken.encoding_for_model(model_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
