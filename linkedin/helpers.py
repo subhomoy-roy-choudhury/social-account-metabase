c213d7a667e5c0a145538588e28283173cccbdf4
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2023-11-08 15:36:14 +0530

    removed request_timeout form openAI API

--- linkedin/helpers.py
+++ linkedin/helpers.py
@@ -76,7 +76,7 @@ def generate_summarizer(
         temperature=float(temperature),
         top_p=float(top_p),
         frequency_penalty=float(frequency_penalty),
-        request_timeout=15,
+        # request_timeout=15,
         messages=[
             {
                 "role": "system",
